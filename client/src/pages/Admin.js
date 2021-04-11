edeb5e23265d4538c9e7dc97de513c6f08065fbb
michaelolson1996 <michaelolson.1996@protonmail.com>
2021-04-11 17:14:29 -0600

    initial commit

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AdminPage() {
+    return (
+        <div>
+            <div>
+                
+            </div>    
+        </div>
+    )
+}
