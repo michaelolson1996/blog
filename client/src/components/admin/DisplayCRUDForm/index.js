162b681484337eefb8553ef64e279636ebc11686
michaelolson1996 <michaelolson.1996@protonmail.com>
2021-05-01 10:57:13 -0600

    added post sections to state

--- client/src/components/admin/DisplayCRUDForm/index.js
+++ client/src/components/admin/DisplayCRUDForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 class DisplayCRUDForm extends React.Component {
@@ -8,14 +8,21 @@ class DisplayCRUDForm extends React.Component {
             dropdown: {
                 display: false
             },
+            blogItemForm: {
+                display: false,
+                blogItem: ''
+            },
             categories: [],
             chosenCategory: '',
             post: {
                 title: '',
+                subTitle: '',
+                dateCreated: '',
                 headerImage: {
                     raw: '',
                     preview: ''
-                }
+                },
+                content: []
             }
         }
     }
@@ -34,7 +41,6 @@ class DisplayCRUDForm extends React.Component {
             }));
         }
     }
-
     
     displayDropdown = e => {
         e.preventDefault();
@@ -58,28 +64,194 @@ class DisplayCRUDForm extends React.Component {
     handlePostTitle = e => {
         e.preventDefault()
 
-        this.setState({
+        this.setState(oldState => ({
             post: {
-                title: e.target.value
+                ...oldState.post,
+                title: e.target.value,
             }
-        })
+        }))
+    }
+
+    handlePostSubtitle = e => {
+        e.preventDefault()
+
+        this.setState(oldState => ({
+            ...oldState.post,
+            subTitle: e.target.value,
+        }))
+    }
+
+    handleHeaderImage = e => {
+        let reader = new FileReader();
+
+        reader.readAsDataURL(e.target.files[0]);
+
+        reader.onload = () => {
+            this.setState(oldState => ({
+                post: {
+                    ...oldState.post,
+                    headerImage: {
+                        raw: reader.result,
+                        preview: URL.createObjectURL(e.target.files[0])
+                    }
+                }
+            }))
+        }
+    }
+
+    submitData = (e, data) => {
+        e.preventDefault()
+
+        this.setState(oldState => ({
+            ...oldState,
+            blogItemForm: {
+                ...oldState.blogItemForm,
+                display: !oldState.blogItemForm.display
+            },
+            post: {
+                ...oldState.post,
+                content: [...oldState.post.content, data],
+            }
+        }));
+    }
+
+    addParagraph = () => {
+        return (
+            <>
+                <h2>Paragraph</h2>
+                <textarea id='paragraph-data' cols='60' rows='7' />
+                <button onClick={ (e) => this.submitData(e, {type: 'paragraph', value: document.getElementById('paragraph-data').value}) }>Submit</button>
+            </>
+        );
+    }
+
+    addImage = () => {
+        return (
+            <>
+                <h2>Image</h2>
+
+            </>
+        );
+    }
+
+    addCode = () => {
+        return (
+            <>
+                <h2>Code</h2>
+                <textarea id='code-data' cols='60' rows='7' />
+                <button onClick={ (e) => this.submitData(e, {type: 'code', value: document.getElementById('code-data').value}) }>Submit</button>
+            </>
+        );
+    }
+
+    addVideo = () => {
+        return (
+            <>
+                <h2>Video</h2>
+            </>
+        );
+    }
+
+    addSeperator = () => {
+        return (
+            <>
+                <h2>Seperator</h2>
+            </>
+        );
+    }
+
+    addSectionTitle = () => {
+        return (
+            <>
+                <h2>Section Title</h2>
+            </>
+        );
+    }
+
+    toggleFormDisplay = e => {
+        e.preventDefault();
+
+        this.setState(oldState => ({
+            blogItemForm: {
+                display: !oldState.blogItemForm.display
+            }
+        }))
+    }
+
+    setFormItem = () => {
+        let formData;
+        switch (this.state.blogItemForm.blogItem) {
+            case 'Paragraph': {
+                formData = this.addParagraph();
+                break;
+            }
+            case 'Image': {
+                formData = this.addImage();
+                break;
+            }
+            case 'Code': {
+                formData = this.addCode();
+                break;
+            }
+            case 'Video': {
+                formData = this.addVideo();
+                break;
+            }
+            case 'Seperator': {
+                formData = this.addSeperator();
+                break;
+            }
+            case 'Section Title': {
+                formData = this.addSectionTitle();
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+        // return formData;
+
+        return (
+            <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                { formData }
+            </div>
+        )
+    }
+
+    displayBlogItemForm = e => {
+        e.preventDefault();
+
+        this.setState(oldState => ({
+            blogItemForm: {
+                display: !oldState.blogItemForm.display,
+                blogItem: e.target.textContent
+            }
+        }))
     }
 
     render() {
         return (
-            <form style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <form style={{ width: '100%', height: '100%', overflowY: 'auto' }}>
                 <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '30px' }}>
-                    <label>
-                        Post Title
-                        <input style={{ height: '40px', marginLeft: '20px' }} onChange={ this.handlePostTitle } type='text' placeholder='title' />
-                    </label>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'space-around', height: '150px' }}>
+                        <label>
+                            Post Title
+                            <input style={{ height: '40px', marginLeft: '20px' }} onChange={ this.handlePostTitle } type='text' placeholder='title' />
+                        </label>
+                        <label>
+                            Post SubTitle
+                            <input style={{ height: '40px', marginLeft: '20px' }} onChange={ this.handlePostSubtitle } type='text' placeholder='subtitle' />
+                        </label>
+                    </div>
+
                     <div style={{ height: 'auto', display: 'flex', flexDirection: 'column' }}>
                         <button onClick={ this.displayDropdown } style={{ height: '40px', width: '280px' }}>{ this.state.chosenCategory ? this.state.chosenCategory : 'Pick Category'}</button>
                         {
                             (this.state.dropdown.display && this.state.categories) ?
                                 this.state.categories.map((category, i) => {
                                     return (
-                                        <button onClick={ () => this.chooseCategory(category.title) } key={i} style={{ display: 'flex', alignItems: 'center', height: '40px', width: '280px' }}>
+                                        <button onClick={ () => this.chooseCategory(category.title) } key={i} style={{ display: 'flex', alignItems: 'center', height: '40px', width: '280px', position: 'relative' }}>
                                             <img style={{ height: '30px', width: '30px', marginRight: '30px' }} alt={ category.title } src={ `data:image/png;base64,${ category.image }` } />
                                             <p>{ category.title }</p>
                                         </button>
@@ -90,15 +262,42 @@ class DisplayCRUDForm extends React.Component {
                         }
                     </div>
                 </div>
-                <div>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '30px 0 30px 0' }}>
                     <label htmlFor='header-image'>
-
+                        {
+                            this.state.post.headerImage.preview ?
+                                <img style={{ minHeight: '400px', width: '90%' }} src={this.state.post.headerImage.preview} alt="dummy" width="300" height="300" />
+                            :
+                                <div style={{ minHeight: '400px', width: '90%' }}></div>
+                        }
                     </label>
-                    <input type='file' id='header-image' accept='image/*'>Upload Header Image</input>
+                    <input onChange={ this.handleHeaderImage } type='file' id='header-image' accept='image/*' style={{ marginTop: '30px' }} />
                 </div>
                 {
-                    console.log(this.state)
+                    this.state.blogItemForm.display ?
+                        <>
+                            <div id='blog-item-form' style={{ height: '90vh', width: '90vw', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)', backgroundColor: 'white', border: '1px solid black', zIndex: 4 }}>
+                                {
+                                    this.setFormItem()
+                                }
+                            </div>
+                            <div onClick={ this.toggleFormDisplay } id='blog-item-form-bg' style={{ height: '100vh', width: '100vw', position: 'absolute', top: '0', left: '0', backgroundColor: 'rgba(0,0,0,0.5)', zIndex: 3 }}></div>
+                        </>
+                    :
+                        <></>
                 }
+
+                <div style={{ width: '100%', display: 'flex', justifyContent: 'space-around' }}>
+                        <button onClick={ this.displayBlogItemForm }>Paragraph</button>
+                        <button onClick={ this.displayBlogItemForm }>Image</button>
+                        <button onClick={ this.displayBlogItemForm }>Code</button>
+                        <button onClick={ this.displayBlogItemForm }>Video</button>
+                        <button onClick={ this.displayBlogItemForm }>Seperator</button>
+                        <button onClick={ this.displayBlogItemForm }>Section Title</button>
+                </div>
+                <div className='blog-container' style={{ width: '100%', height: 'auto' }}>
+
+                </div>
             </form>
         )
     }
