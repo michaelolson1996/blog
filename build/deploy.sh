1af17f4c8cd7ef555d15cb02432e2084839f54b0
michaelolson1996 <michaelolson.1996@protonmail.com>
2021-06-12 13:12:14 -0600

    added scp command to send code to ec2 instance

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,4 +3,6 @@
 ls
 tar -zcvf michaelolsonblog.tar.gz ./src/
 
+scp michaelolsonblog.tar.gz ec2-user@michaelolson.blog:/home/ec2-user
+
 ssh ec2-user@michaelolson.blog tar -xvzf michaelolsonblog.tar.gz
