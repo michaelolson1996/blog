4b175730828540e69ef711c9e6f8d50ce64233b8
michaelolson1996 <michaelolson.1996@protonmail.com>
2021-06-15 08:40:54 -0600

    created bash script full deploy setup

--- build/deploy.sh
+++ build/deploy.sh
@@ -5,4 +5,20 @@ tar -zcvf michaelolsonblog.tar.gz ./src/
 
 scp michaelolsonblog.tar.gz ec2-user@michaelolson.blog:/home/ec2-user
 
-ssh ec2-user@michaelolson.blog tar -xvzf michaelolsonblog.tar.gz
+ssh ec2-user@michaelolson.blog << HERE
+
+tar -xvzf michaelolsonblog.tar.gz
+
+cd michaelolson.blog
+
+docker-compose down
+
+cd .. && rm -r michaelolson.blog
+
+mv src michaelolson.blog
+
+cp .env michaelolson.blog
+
+cd michaelolson.blog && docker-compose upp --build -d
+
+HERE
