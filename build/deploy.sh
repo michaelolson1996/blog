98db1b659a693264d9b383567599c4c26bcfdb0b
michaelolson1996 <michaelolson.1996@protonmail.com>
2021-06-15 08:59:23 -0600

    attempting to open ports depending on circleci machine ip

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-ls
+public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
+echo "this computers public ip address is $public_ip_address"
+aws ec2 authorize-security-group-ingress --region $AWS_REGION --group-id $AWS_SEC_GROUP_ID --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": ${CIRCLECI_VM_PORT}, \"ToPort\": ${AWS_VM_PORT}, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
+
 tar -zcvf michaelolsonblog.tar.gz ./src/
 
 scp michaelolsonblog.tar.gz ec2-user@michaelolson.blog:/home/ec2-user
