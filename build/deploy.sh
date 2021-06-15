448910e0b0eca45a92538ebb6f438e0b7a8eb2a1
michaelolson1996 <michaelolson.1996@protonmail.com>
2021-06-15 10:31:36 -0600

    added check for http / http2 to remain open

--- build/deploy.sh
+++ build/deploy.sh
@@ -38,13 +38,15 @@ current_security_group=$(aws ec2 describe-security-groups --region ${AWS_REGION}
 ip_count=$(echo ${current_security_group} | jq -r '.SecurityGroups[0].IpPermissions | length')
 if [ ${ip_count} > 0 ]; then
     for (( n=0; n < $ip_count; n++ ))
-    do
-        this_port=$(echo ${current_security_group} | jq -r ".SecurityGroups[0].IpPermissions[${n}].FromPort")
-        cidr_count=$(echo ${current_security_group} | jq -r ".SecurityGroups[0].IpPermissions[${n}].IpRanges | length")
-        for (( c=0; c < $cidr_count; c++ ))
-            do
-                this_cidr=$(echo ${current_security_group} | jq -r ".SecurityGroups[0].IpPermissions[${n}].IpRanges[${c}].CidrIp")
-                aws ec2 revoke-security-group-ingress --region ${AWS_REGION} --group-id ${AWS_SEC_GROUP_ID} --protocol tcp --port ${this_port} --cidr ${this_cidr}
+        do
+            this_port=$(echo ${current_security_group} | jq -r ".SecurityGroups[0].IpPermissions[${n}].FromPort")
+            cidr_count=$(echo ${current_security_group} | jq -r ".SecurityGroups[0].IpPermissions[${n}].IpRanges | length")
+            for (( c=0; c < $cidr_count; c++ ))
+                if [ ${this_port} != 80 && ${this_port} != 443 ]; then
+                    do
+                        this_cidr=$(echo ${current_security_group} | jq -r ".SecurityGroups[0].IpPermissions[${n}].IpRanges[${c}].CidrIp")
+                        aws ec2 revoke-security-group-ingress --region ${AWS_REGION} --group-id ${AWS_SEC_GROUP_ID} --protocol tcp --port ${this_port} --cidr ${this_cidr}
+                fi
         done
     done
 fi
